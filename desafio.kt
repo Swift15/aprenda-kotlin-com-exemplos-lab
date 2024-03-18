b28ae4eb8a00d405e6eaf3ee8b4d622c11a9674c
Swift15 <30559274+Swift15@users.noreply.github.com>
2024-03-18 18:06:17 -0300

    Primeiro Ajuste

--- desafio.kt
+++ desafio.kt
@@ -1,14 +1,20 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario
+//data class
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+
+//verificar se var ou val
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
+    
+    
+    //utilizar enum class Nivel 
     
     fun matricular(usuario: Usuario) {
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
